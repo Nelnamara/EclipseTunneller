26188838ba44905dce8260aa58b20b17bb271ab3
Nelnamara <gwilkins@local>
2026-06-11 22:46:34 -0700

    Initial commit: EclipseTunneller skeleton

---
+++ EclipseTunneller.lua
@@ -0,0 +1,8 @@
+-- EclipseTunneller
+-- Balance Druid Eclipse HUD for WoW Midnight (12.x)
+-- Author: Nelnamara
+
+EclipseTunneller = {}
+local ET = EclipseTunneller
+
+-- TODO: implement Eclipse state tracking, DoT timers, cast window HUD
